f9bd752d8ed1c073b72148b3cc8515a080370050
Denis Alessandro Altoe Falqueto <denisf@trt3.jus.br>
2019-09-10 15:20:57 -0300

    Criar operações de débito e crédito na conta

--- src/main/java/br/jus/trt3/curso/aula2/homebanking/Conta.java
+++ src/main/java/br/jus/trt3/curso/aula2/homebanking/Conta.java
@@ -1,6 +1,7 @@
 package br.jus.trt3.curso.aula2.homebanking;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -26,5 +27,19 @@ public class Conta {
 
     public List<Movimento> getMovimentos() {
         return Collections.unmodifiableList(movimentos);
+    }
+
+    public void depositar(LocalDate data, BigDecimal valor) {
+        if (valor.compareTo(BigDecimal.valueOf(0d)) < 0) {
+            throw new IllegalArgumentException("O valor deve ser positivo");
+        }
+        movimentos.add(new Movimento(data, valor));
+    }
+
+    public void sacar(LocalDate data, BigDecimal valor) {
+        if (valor.compareTo(BigDecimal.valueOf(0d)) < 0) {
+            throw new IllegalArgumentException("O valor deve ser positivo");
+        }
+        movimentos.add(new Movimento(data, valor.multiply(BigDecimal.valueOf(-1d))));
     }
 }
