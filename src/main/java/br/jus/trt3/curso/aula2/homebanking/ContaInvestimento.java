a3136fbecc229255636134872016cfbf5e961093
Denis Alessandro Altoe Falqueto <denisf@trt3.jus.br>
2019-09-10 14:53:04 -0300

    Criação da conta de investimento

---
+++ src/main/java/br/jus/trt3/curso/aula2/homebanking/ContaInvestimento.java
@@ -0,0 +1,20 @@
+package br.jus.trt3.curso.aula2.homebanking;
+
+import java.math.BigDecimal;
+
+/**
+ * ContaAplicacao
+ */
+public class ContaInvestimento extends Conta {
+
+    private final TipoAplicacao tipoAplicacao;
+
+    public ContaInvestimento(Long id, BigDecimal saldo, TipoAplicacao tipoAplicacao) {
+        super(id, saldo);
+        this.tipoAplicacao = tipoAplicacao;
+    }
+
+    public TipoAplicacao getTipoAplicacao() {
+        return tipoAplicacao;
+    }
+}
---
+++ src/main/java/br/jus/trt3/curso/aula2/homebanking/TipoAplicacao.java
@@ -0,0 +1,17 @@
+package br.jus.trt3.curso.aula2.homebanking;
+
+/**
+ * TipoAplicacao
+ */
+public class TipoAplicacao {
+
+    private final String descricao;
+
+    public TipoAplicacao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
