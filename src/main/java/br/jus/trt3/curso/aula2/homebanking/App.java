be698894d48a9b9ee679211044fbedd4e385cef1
Denis Alessandro Altoe Falqueto <denisf@trt3.jus.br>
2019-09-10 14:39:35 -0300

    Commit Inicial

---
+++ src/main/java/br/jus/trt3/curso/aula2/homebanking/App.java
@@ -0,0 +1,19 @@
+package br.jus.trt3.curso.aula2.homebanking;
+
+/**
+ * Hello world!
+ */
+public final class App {
+
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
